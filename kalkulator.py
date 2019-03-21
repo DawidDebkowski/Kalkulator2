e95ee6bd89913f3da2b51103c71c2218625c03fa
DawidDebkowski <dawid.debo2.pl@gmail.com>
2019-03-21 19:04:26 +0100

    działanie operacji

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,7 @@
 def calc_add(a, b):
   return a+b
 
-def calc_subtract(a, b):
+def calc_substract(a, b):
   return a-b
 
 def calc_multiple(a, b):
@@ -10,12 +10,15 @@ def calc_multiple(a, b):
 def calc_divide(a, b):
   return a / b 
 def check_operation_name(operation):
-  while True:
-    pass
+  if operation in method_dict:
+    return method_dict[operation]
+  operation = input("Niepoprawne dzialanie, podaj jeszce raz: dodawanie, odjemowanie, mnozenie lub dzielenie: ")
+  check_operation_name(operation)
+
 
 method_dict = {
   "dodawanie" : calc_add,
-  "odejmowanie" : calc_subtract,
+  "odejmowanie" : calc_substract,
   "mnozenie" : calc_multiple,
   "dzielenie" : calc_divide
 }
@@ -23,4 +26,7 @@ method_dict = {
 if __name__ == "__main__":
   print("Witam w moim kalkulatorze :P")
   operation = input("Podaj dzialanie: dodawanie, odjemowanie, mnozenie lub dzielenie: ")
-  print(operation)
+  operation_def = check_operation_name(operation)
+  a = int(input("Podaj pierwsza liczbe: "))
+  b = int(input("Podaj druga liczbe: "))
+  print(operation_def(a, b))
