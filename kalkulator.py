07327d903da771242af2b0a84a1decd69e550031
DawidDebkowski <dawid.debo2.pl@gmail.com>
2019-03-21 18:46:55 +0100

    dodanie słownika

--- kalkulator.py
+++ kalkulator.py
@@ -9,6 +9,18 @@ def calc_multiple(a, b):
 
 def calc_divide(a, b):
   return a / b 
+def check_operation_name(operation):
+  while True:
+    pass
+
+method_dict = {
+  "dodawanie" : calc_add,
+  "odejmowanie" : calc_subtract,
+  "mnozenie" : calc_multiple,
+  "dzielenie" : calc_divide
+}
 
 if __name__ == "__main__":
-  print("Witam w moim kalkulatorze :D")
+  print("Witam w moim kalkulatorze :P")
+  operation = input("Podaj dzialanie: dodawanie, odjemowanie, mnozenie lub dzielenie: ")
+  print(operation)
