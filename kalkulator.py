a80894fcb2b3bd95c1f0c7e40f96841e3d7e695b
DawidDebkowski <dawid.debo2.pl@gmail.com>
2019-03-21 18:33:02 +0100

    4 działania i powitanie

---
+++ kalkulator.py
@@ -0,0 +1,14 @@
+def calc_add(a, b):
+  return a+b
+
+def calc_subtract(a, b):
+  return a-b
+
+def calc_multiple(a, b):
+  return a*b
+
+def calc_divide(a, b):
+  return a / b 
+
+if __name__ == "__main__":
+  print("Witam w moim kalkulatorze :D")
